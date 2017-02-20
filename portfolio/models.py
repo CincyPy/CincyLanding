b9f8667365654ffd77f8c562bc87c609512458d4
Chris <chris@internodal.xyz>
2017-02-20 10:07:46 -0500

    Made portfolio model class

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Package(models.Model):
+    name = models.CharField(max_length=100)
+    docs_link = models.URLField()
+
+
+class PortfolioItem(models.Model):
+    STATUS_CHOICES = (
+        ('D', 'Designing'),
+        ('A', 'Active'),
+        ('C', 'Complete'),
+    )
+
+    project_name = models.CharField(primary_key=True, max_length=100)
+    project_description = models.TextField()
+    picture = models.ImageField()
+    url = models.URLField()
+    start_date = models.DateField()
+    github_link = models.URLField()
+    trello_link = models.URLField()
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    packages = models.ForeignKey(Package, on_delete=models.CASCADE)
