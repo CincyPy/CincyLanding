023c839f61ee447aeb1c36d17e6c3426c71bcbce
Chris <chris@internodal.xyz>
2017-02-23 18:44:52 -0500

    added tests to check home, tutorial, and admin url's with selenium

--- portfolio/tests.py
+++ portfolio/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from selenium import webdriver
+
 
 # Create your tests here.
+class UrlTest(TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_home_url(self):
+        self.browser.get('http://localhost:8000/home/')
+        self.assertIn('Start', self.browser.title)
+
+    def test_portfolio_url(self):
+        self.browser.get('http://localhost:8000/portfolio/')
+        self.assertIn('Start', self.browser.title)
+
+    def test_admin_url(self):
+        self.browser.get('http://localhost:8000/admin/')
+        self.assertIn('admin', self.browser.title)
+
