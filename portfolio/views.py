9504fb9844e15a0d303045d7e5e403ed976fdd2d
Chris <chris@internodal.xyz>
2017-03-02 21:02:11 -0500

    Changed Model to only require name, url, and description

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,24 +1,16 @@
 from django.views.generic.detail import DetailView
 from django.views.generic import TemplateView
 from .models import *
-from django.utils import timezone
-# Create your views here.
 
 
 class PortfolioView(TemplateView):
     template_name = "portfolio/portfolio.html"
 
 
-class PortfolioDetails(TemplateView):
+class PortfolioDetailView(DetailView):
+    model = PortfolioItem
     template_name = "portfolio/portfolio_details.html"
 
     def get_context_data(self, **kwargs):
-        context = super(PortfolioDetails, self).get_context_data(**kwargs)
-        portid = kwargs.get('pk')
-        context['portfolio_details'] = self.get_port_details(portid)
-        return context
-
-    def get_port_details(self, portid):
-        context = {}
-        context['me'] = portid
+        context = super(PortfolioDetailView, self).get_context_data(**kwargs)
         return context
